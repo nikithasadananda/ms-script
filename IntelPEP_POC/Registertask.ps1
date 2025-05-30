2572d873c0f2b067d086db3762fceb6e0da20b78
nikithasadananda <nikithax.sadananda@intel.com>
2025-05-30 15:58:56 -0700

    Update IntelPEP_POC

--- IntelPEP_POC/Registertask.ps1
+++ IntelPEP_POC/Registertask.ps1
@@ -1,5 +1,5 @@
 $taskName = "IntelPepIntegration"
-$scriptPath = "$env:USERPROFILE\Desktop\IntelPepIntegration.ps1"
+$scriptPath = "$env:USERPROFILE\Desktop\Intelpep.ps1"
 
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`""
 $trigger = New-ScheduledTaskTrigger -AtStartup

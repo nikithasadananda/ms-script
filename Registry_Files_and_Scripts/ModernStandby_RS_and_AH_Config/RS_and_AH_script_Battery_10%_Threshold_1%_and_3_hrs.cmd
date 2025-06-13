0642ff5ad014103b22ecede5698639455d833444
nikithasadananda <nikithax.sadananda@intel.com>
2025-06-13 10:48:13 -0700

    Update RS_and_AH_script_Battery_10%_Threshold_1%_and_3_hrs.cmd

--- Registry_Files_and_Scripts/ModernStandby_RS_and_AH_Config/RS_and_AH_script_Battery_10%_Threshold_1%_and_3_hrs.cmd
+++ Registry_Files_and_Scripts/ModernStandby_RS_and_AH_Config/RS_and_AH_script_Battery_10%_Threshold_1%_and_3_hrs.cmd
@@ -1,22 +1,24 @@
 @echo off
-:: RS_AH_Validation.bat - Validates AH behavior when battery reaches 10%
+:: RS_AH_Validation.cmd - Validates AH behavior when battery reaches 10%
 
 :: Check for admin rights
 NET FILE 1>NUL 2>NUL
 if '%errorlevel%' == '0' (
-    goto continue
+    goto continueToRun
 ) else (
     powershell "Start-Process -FilePath '%~f0' -Verb RunAs" >nul 2>&1
     exit /b
 )
 
-:continue
+:continueToRun
 
 :: Wait until battery is at or below 10%
-echo Waiting for battery to reach 10%%...
+echo ------------------------------------------------------------
+echo Waiting for battery to reach 10%% before continuing...
+echo ------------------------------------------------------------
+
 :checkBattery
-for /f "tokens=2 delims==" %%i in ('"wmic path Win32_Battery get EstimatedChargeRemaining /value"') do set battery=%%i
-set /a battery=%battery%
+for /f %%i in ('powershell -command "(Get-CimInstance -ClassName Win32_Battery).EstimatedChargeRemaining"') do set battery=%%i
 echo Battery: %battery%%%
 
 if %battery% GTR 10 (
@@ -30,6 +32,6 @@ powercfg /setdcvalueindex SCHEME_CURRENT 8619b916-e004-4dd8-9b66-dae86f806698 9f
 powercfg /setdcvalueindex SCHEME_CURRENT 8619b916-e004-4dd8-9b66-dae86f806698 61f45dfe-1919-4180-bb46-8cc70e0b38f1 10800
 powercfg -setactive SCHEME_CURRENT
 
-echo AH Threshold set to 1%%, Window = 3 hours
+echo ---------AH Battery Drain Threshold set to 1%% and AH Window Length set to 3 hours---------
 pause
 exit
